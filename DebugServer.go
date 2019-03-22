e49deb954a43ba836e6d812b155283bd072a124d
Renzo Crisostomo <renzo@crisostomo.me>
2019-03-22 21:44:14 +0100

    Add DebugServer

---
+++ DebugServer.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net"
+)
+
+type ScannerState byte
+
+const (
+	Beginning ScannerState = '$'
+	End       ScannerState = '#'
+)
+
+var (
+	state = Beginning
+)
+
+func startDebugServer(port uint) {
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		fmt.Println("Error listening: ", err.Error())
+	}
+	defer lis.Close()
+	conn, err := lis.Accept()
+	if err != nil {
+		fmt.Println("Error accepting: ", err.Error())
+	}
+	for {
+		handleConnection(conn)
+	}
+}
+
+func scan(data []byte, atEOF bool) (int, []byte, error) {
+	for i := 0; i < len(data); i++ {
+		if data[i] == byte(state) {
+			if state == Beginning {
+				state = End
+			} else {
+				state = Beginning
+			}
+			return i + 1, data[:i], nil
+		}
+	}
+	return 0, data, bufio.ErrFinalToken
+}
+
+func validatePacket(packet string, checksum string) bool {
+	return true
+}
+
+func ack(conn net.Conn) {
+	conn.Write([]byte("+"))
+}
+
+func nack(conn net.Conn) {
+	conn.Write([]byte("-"))
+}
+
+func handleConnection(conn net.Conn) {
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return
+	}
+	scanner := bufio.NewScanner(bytes.NewReader(buf[:n]))
+	scanner.Split(scan)
+	for scanner.Scan() {
+		packet := scanner.Text()
+		switch packet {
+		case "":
+		case "+":
+		case "-":
+		default:
+			scanner.Scan()
+			checkSum := scanner.Text()
+			if validatePacket(packet, checkSum) {
+				ack(conn)
+				reply(conn, packet)
+			} else {
+				nack(conn)
+			}
+		}
+	}
+}
+
+func send(conn net.Conn, message string) {
+	checksum := uint8(0)
+	for i := 0; i < len(message); i++ {
+		char := message[i]
+		checksum += char
+	}
+	reply := fmt.Sprintf("$%s#%02x", message, checksum)
+	conn.Write([]byte(reply))
+}
+
+func reply(conn net.Conn, packet string) {
+	send(conn, "")
+}
